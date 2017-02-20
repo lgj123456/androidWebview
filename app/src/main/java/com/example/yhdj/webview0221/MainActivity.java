346293d7642f4346f43b5294da36cdaf057183ea
yhdj <1079652907@qq.com>
2017-02-21 00:15:58 +0800

    在程序内打开网页

--- app/src/main/java/com/example/yhdj/webview0221/MainActivity.java
+++ app/src/main/java/com/example/yhdj/webview0221/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.yhdj.webview0221;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WebView mWebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        mWebView = (WebView) findViewById(R.id.webView);
+        mWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+               view.loadUrl(url);
+                return true;
+            }
+        });
+
+        mWebView.loadUrl("http://www.baidu.com");
     }
 }
