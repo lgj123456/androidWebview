ad7976c3e289a637680576a1a5d4b90422a90b49
yhdj <1079652907@qq.com>
2017-02-21 00:21:45 +0800

    判断页面加载过程

--- app/src/main/java/com/example/yhdj/webview0221/MainActivity.java
+++ app/src/main/java/com/example/yhdj/webview0221/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.yhdj.webview0221;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +26,20 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+        mWebView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                if(newProgress == 100){
+                    Toast.makeText(MainActivity.this, "网页加载完成！！！", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "网页加载中······", Toast.LENGTH_SHORT).show();
+                }
+
+                super.onProgressChanged(view, newProgress);
+            }
+        });
+
 
         mWebView.loadUrl("http://www.baidu.com");
     }
